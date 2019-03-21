67ae1eb93b189487225249bdfb845fb02483c73b
Bernadette Estacio <bernadette@englemaninvest.com>
2019-03-20 22:02:33 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,286 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import start from "./img/start.png";
+import bubbleImg from "./img/bubble.jpg";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      styleDisplay: { display: "block" },
+      styleVisibility: { visibility: "hidden" },
+      timeLeft: 30,
+      // Randomly Select the Burst-all Bubble
+      pickedBubble: "bubble" + Math.floor(Math.random() * 64 + 1)
+    };
+  }
+
+  startGame = () => {
+    console.log(
+      ReactDOM.findDOMNode(document.getElementById(this.state.pickedBubble))
+    ); // DELETE------------
+    this.setState({
+      styleDisplay: { display: "none" },
+      styleVisibility: { visibility: "visible" }
+    });
+    this.timerId();
+  };
+
+  timerId = () => {
+    setInterval(
+      function() {
+        this.state.timeLeft === 0
+          ? this.stopGame()
+          : this.setState({ timeLeft: this.state.timeLeft - 1 });
+      }.bind(this),
+      1000
+    );
+  };
+
+  clickBubble = ev => {
+    let clickedBubble = ev.currentTarget.id;
+    this.hideBubble(clickedBubble);
+    if (clickedBubble === this.state.pickedBubble) {
+      this.burstBonus();
+    }
+    switch (clickedBubble) {
+      case "bubble6":
+        this.showBubbles("popbubble6");
+        break;
+      case "bubble11":
+        this.showBubbles("popbubble11");
+        break;
+      case "bubble24":
+        this.showBubbles("popbubble24");
+        break;
+      case "bubble30":
+        this.showBubbles("popbubble30");
+        break;
+      case "bubble43":
+        this.showBubbles("popbubble43");
+        break;
+      case "bubble52":
+        this.showBubbles("popbubble52");
+        break;
+      case "bubble59":
+        this.showBubbles("popbubble59");
+        break;
+    }
+  };
+
+  hideBubble = clickedBubble => {
+    ReactDOM.findDOMNode(
+      document.getElementById(clickedBubble)
+    ).style.visibility = "hidden";
+  };
+
+  showBubbles = showBubble => {
+    ReactDOM.findDOMNode(
+      document.getElementsByClassName(showBubble)
+    ).style.visibility = "visible";
+  };
+
+  // Burst all bubble by pickedBubble
+  burstBonus = () => {
+    this.burst();
+    setTimeout(function() {
+      ReactDOM.findDOMNode(
+        document.getElementById("instruction")
+      ).style.visibility = "hidden";
+      ReactDOM.findDOMNode(document.getElementById("timeUP")).style.display =
+        "none";
+      ReactDOM.findDOMNode(
+        document.getElementById("goodjob")
+      ).style.visibility = "visible";
+    }, 750);
+  };
+
+  stopGame = () => {
+    this.burst();
+    // ReactDOM.findDOMNode(
+    //   document.getElementById("instruction")
+    // ).style.visibility = "hidden";
+    ReactDOM.findDOMNode(document.getElementById("timeUP")).style.visibility =
+      "visible";
+  };
+
+  // Burst All Bubbles
+  burst = () => {
+    ReactDOM.findDOMNode(document.getElementById("allBubbles")).style.display =
+      "none";
+  };
+
+  reset = () => {
+    window.location.reload();
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+      <div>
+        <nav>
           <a
-            className="App-link"
-            href="https://reactjs.org"
+            href="http://bernadetteengleman.com/index.html"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Home
           </a>
-        </header>
+          <span onClick={this.reset}>Reset</span>
+
+          <h1>Pop The Bubbles Game</h1>
+        </nav>
+
+        <main>
+          <div className="row" id="instruction">
+            <p>
+              Some bubbles will make popped bubbles appear BUT there is one
+              bubble that can pop everything!
+            </p>
+
+            <Timer timeLeft={this.state.timeLeft} />
+          </div>
+
+          <div>
+            <img
+              src={start}
+              alt="START"
+              id="startButton"
+              width="80"
+              style={this.state.styleDisplay}
+              onClick={this.startGame}
+            />
+          </div>
+
+          {/* Winner Image */}
+          <div id="goodjob" />
+
+          {/* Time is Up Image */}
+          <div id="timeUP" />
+
+          <Bubbles
+            bubbles={this.props.bubbles}
+            styleVisibility={this.state.styleVisibility}
+            clickBubble={this.clickBubble}
+          />
+        </main>
+
+        <footer>
+          <p>
+            <small>
+              Copyright © 2015-{new Date().getFullYear()} Bernadette Estacio.
+              All rights reserved
+            </small>
+          </p>
+        </footer>
       </div>
     );
   }
 }
 
+const Timer = props => {
+  return (
+    <div>
+      <p>You have 30 seconds to pop all the bubbles.</p>
+
+      <p className="timer">
+        Time Left:{" "}
+        <strong>
+          <span id="countDown">{props.timeLeft}</span>{" "}
+        </strong>
+        sec
+      </p>
+    </div>
+  );
+};
+
+const Bubbles = props => {
+  return (
+    <div id="allBubbles">
+      {props.bubbles.map((bubble, i) => (
+        <div key={i}>
+          <img
+            src={bubbleImg}
+            alt="bubble"
+            id={"bubble" + (i + 1)}
+            className={bubble}
+            style={props.styleVisibility}
+            onClick={props.clickBubble}
+            unselectable="on"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+App.defaultProps = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  bubbles: [
+    "pop",
+    "popbubble43",
+    "popbubble11",
+    "popbubble30",
+    "popbubble59",
+    "pop",
+    "popbubble52",
+    "popbubble24",
+    "popbubble43",
+    "pop",
+    "pop",
+    "popbubble6",
+    "popbubble24",
+    "popbubble52",
+    "popbubble59",
+    "pop",
+    "popbubble43",
+    "pop",
+    "pop",
+    "popbubble52",
+    "popbubble30",
+    "popbubble59",
+    "popbubble6",
+    "pop",
+    "popbubble11",
+    "popbubble52",
+    "popbubble43",
+    "pop",
+    "popbubble24",
+    "pop",
+    "popbubble59",
+    "pop",
+    "popbubble24",
+    "popbubble6",
+    "popbubble59",
+    "popbubble52",
+    "pop",
+    "popbubble43",
+    "popbubble59",
+    "popbubble30",
+    "popbubble52",
+    "popbubble24",
+    "pop",
+    "popbubble30",
+    "popbubble6",
+    "pop",
+    "pop",
+    "popbubble59",
+    "pop",
+    "popbubble24",
+    "popbubble6",
+    "pop",
+    "popbubble11",
+    "popbubble43",
+    "pop",
+    "pop",
+    "popbubble11",
+    "popbubble52",
+    "pop",
+    "popbubble24",
+    "popbubble30",
+    "popbubble6",
+    "popbubble43",
+    "pop"
+  ]
+};
 export default App;
